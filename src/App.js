3678dfcb16e3b9fb4bc46d9a3727e8dfd78d6c03
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-09-26 08:25:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <User />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+class User extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: '',
+      error: '',
+    };
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  dismissError = () => {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password is required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+  handleUserChange = (evt) => {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  handlePassChange = (evt) => {
+    this.setState({
+      password: evt.target.value,
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          {
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+          <label>User Name</label>
+          <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+
+          <label>Password</label>
+          <input type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+
+          <input type="submit" value="Log In" data-test="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
